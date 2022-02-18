4c8100241ccdef828ba49da18af870d18444cbf0
V7ADI <therion.it@gmail.com>
2022-02-18 23:23:18 +0400

    commit
    commit

---
+++ src/main/java/app/model/Cat.java
@@ -0,0 +1,15 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat extends Animal {
+
+    public Cat() {
+    }
+
+    @Override
+    public String toString() {
+        return "Im a Cat";
+    }
+}
---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,15 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+
+    public Timer(){}
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
